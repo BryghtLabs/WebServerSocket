e20f39f33a5afa9c28e03a74e0a366f06a298794
Tolu Oridota <toludev@yahoo.com>
2024-02-02 11:24:17 -0600

    added url scheme

--- WebServerSocket/WebServerSocket/Script/socketScript.js
+++ WebServerSocket/WebServerSocket/Script/socketScript.js
@@ -7,81 +7,84 @@
 
 try {
     
-    let socket = new WebSocket("ws://127.0.0.1:1999", "SocketBandit");
-    
     function startSocket() {
         socket = new WebSocket("ws://127.0.0.1:1999", "SocketBandit");
-    }
-    
-    socket.onopen = function(e) {
-        try {
-            alert("[open] Connection established");
-            alert("Sending to server");
-            socket.send("My name is John");
-            const socketOpen = { msgType: 2 }
-            window.webkit.messageHandlers.socketOpen.postMessage(socketOpen);
-            window.console.log = captureLog;
-        } catch (error) {
-            console.error('An open-socket error occurred:', error);
-            const errorEntity = { msgType: 3, msgMonitor: { isError: true, errMsg: "Socket Open Error: " + error.message, msg: "" } }
-            window.webkit.messageHandlers.socketOpenError.postMessage(errorEntity);
-            window.console.log = captureLog;
-        }
-    };
-    
-    socket.onmessage = function(event) {
-        try {
-            alert(`[message] Data received from server: ${event.data}`);
-            const socketOnMsg = { msgType: 4 }
-            window.webkit.messageHandlers.SocketOnMessage.postMessage(socketOnMsg);
-            window.console.log = captureLog;
-        } catch (error) {
-            console.error('An on-msg-socket error occurred:', error);
-            const errorEntity = { msgType: 5, msgMonitor: { isError: true, errMsg: "Socket On Message Error: " + error.message, msg: "" } }
-            window.webkit.messageHandlers.socketOnMessageError.postMessage(errorEntity);
-            window.console.log = captureLog;
-        }
-    };
-    
-    socket.onclose = function(event) {
-        try {
-            if (event.wasClean) {
-                alert(`[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`);
-                const cleanSocketClose = { msgType: 6 }
-                window.webkit.messageHandlers.socketOnClose.postMessage(cleanSocketClose);
+        //socket = new WebSocket("com.bryghtlab.WebServerSocket://127.0.0.1:1999", "SocketBandit");
+        //socket = new WebSocket("ws://localhost", "SocketBandit");
+        
+        socket.onopen = function(e) {
+            try {
+                alert("[open] Connection established");
+                alert("Sending to server");
+                socket.send("My name is John");
+                const socketOpen = { msgType: 2 }
+                window.webkit.messageHandlers.socketOpen.postMessage(socketOpen);
                 window.console.log = captureLog;
-            } else {
-                // e.g. server process killed or network down
-                // event.code is usually 1006 in this case
-                alert('[close] Connection died');
-                const cleanSocketClose = { msgType: 6 }
-                window.webkit.messageHandlers.socketOnClose.postMessage(cleanSocketClose);
+            } catch (error) {
+                console.error('An open-socket error occurred Local:', error);
+                const errorEntity = { msgType: 3, msgMonitor: { isError: true, errMsg: "Socket Open Error: " + error.message, msg: "" } }
+                window.webkit.messageHandlers.socketOpenError.postMessage(errorEntity);
                 window.console.log = captureLog;
             }
-        } catch (error) {
-            console.error('An on-close-socket error occurred:', error);
-            const errorEntity = { msgType: 7, msgMonitor: { isError: true, errMsg: "Socket On Close Error: " + error.message, msg: "" } }
-            window.webkit.messageHandlers.socketOnCloseError.postMessage(errorEntity);
-            window.console.log = captureLog;
-        }
-    };
-    
-    socket.onerror = function(error) {
-        try {
-            const errorObj = JSON.stringify(error);
-            alert(`[error]`);
-            console.error('An on-main-error-socket error occurred:', error);
-            const errorEntity = { msgType: 8, msgMonitor: { isError: true, errMsg: "Socket On Main Error: " + JSON.stringify(error) + ` code = ${error.code}` + ` reason = ${error.reason}`, msg: "" } }
-            window.webkit.messageHandlers.socketMainError.postMessage(errorEntity);
-            window.console.log = captureLog;
-        } catch (errors) {
-            console.error('An on-main-error-socket error occurred in try catch:', errors);
-            const errorEntity = { msgType: 8, msgMonitor: { isError: true, errMsg: "Socket On Main Error: " + JSON.stringify(errors) + ` code = ${error.code}` + ` reason = ${error.reason}`, msg: "" } }
-            window.webkit.messageHandlers.socketMainError.postMessage(errorEntity);
-            window.console.log = captureLog;
-        }
+        };
         
-    };
+        socket.onmessage = function(event) {
+            try {
+                alert(`[message] Data received from server: ${event.data}`);
+                const socketOnMsg = { msgType: 4 }
+                window.webkit.messageHandlers.SocketOnMessage.postMessage(socketOnMsg);
+                window.console.log = captureLog;
+            } catch (error) {
+                console.error('An on-msg-socket error occurred:', error);
+                const errorEntity = { msgType: 5, msgMonitor: { isError: true, errMsg: "Socket On Message Error Local: " + error.message, msg: "" } }
+                window.webkit.messageHandlers.socketOnMessageError.postMessage(errorEntity);
+                window.console.log = captureLog;
+            }
+        };
+        
+        socket.onclose = function(event) {
+            try {
+                if (event.wasClean) {
+                    alert(`[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`);
+                    const cleanSocketClose = { msgType: 6 }
+                    window.webkit.messageHandlers.socketOnClose.postMessage(cleanSocketClose);
+                    window.console.log = captureLog;
+                } else {
+                    // e.g. server process killed or network down
+                    // event.code is usually 1006 in this case
+                    alert('[close] Connection died');
+                    const cleanSocketClose = { msgType: 6 }
+                    window.webkit.messageHandlers.socketOnClose.postMessage(cleanSocketClose);
+                    window.console.log = captureLog;
+                }
+            } catch (error) {
+                console.error('An on-close-socket error occurred:', error);
+                const errorEntity = { msgType: 7, msgMonitor: { isError: true, errMsg: "Socket On Close Error Local: " + error.message, msg: "" } }
+                window.webkit.messageHandlers.socketOnCloseError.postMessage(errorEntity);
+                window.console.log = captureLog;
+            }
+        };
+        
+        socket.onerror = function(error) {
+            try {
+                const errorObj = JSON.stringify(error);
+                //alert(`[error]`);
+                alert(`[${errorObj}]`);
+                console.error('An on-main-error-socket Local:', errorObj);
+                const errorEntity = { msgType: 8, msgMonitor: { isError: true, errMsg: "Socket On Main Error Local: " + JSON.stringify(error) + ` code = ${error.code}` + ` reason = ${error.reason}`, msg: "" } }
+                window.webkit.messageHandlers.socketMainError.postMessage(errorEntity);
+                window.console.log = captureLog;
+            } catch (errors) {
+                console.error('An on-main-error-socket error occurred in try catch:', errors);
+                const errorEntity = { msgType: 8, msgMonitor: { isError: true, errMsg: "Socket On Main Error Local: " + JSON.stringify(errors) + ` code = ${error.code}` + ` reason = ${error.reason}`, msg: "" } }
+                window.webkit.messageHandlers.socketMainError.postMessage(errorEntity);
+                window.console.log = captureLog;
+            }
+            
+        };
+    }
+    
+    
     
     function captureLog(msg) {
         window.webkit.messageHandlers.webViewLogError.postMessage(msg);
@@ -114,6 +117,15 @@ try {
     
     window.onload = function() {
         startSocket()
+    }
+    
+    window.onerror = function (error) {
+        const errorObj = JSON.stringify(error);
+        alert(`[window error: ${errorObj}]`);
+        console.error('A window error occurred Local:', error);
+        const errorEntity = { msgType: 8, msgMonitor: { isError: true, errMsg: "Socket On Window Error Local: " + JSON.stringify(error) + ` code = ${error.code}` + ` reason = ${error.reason}`, msg: "" } }
+        window.webkit.messageHandlers.socketMainError.postMessage(errorEntity);
+        window.console.log = captureLog;
     }
     
 } catch (error) {
